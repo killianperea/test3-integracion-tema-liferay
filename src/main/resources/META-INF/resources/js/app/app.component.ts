bb060f5ae28d810075f45bb3c89028f92a3a4c98
Killian Perea Ruiz <killian.perea@worldline.com>
2018-02-26 12:53:36 +0100

    first commit

---
+++ src/main/resources/META-INF/resources/js/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'app',
+	templateUrl: '/o/test3-integracion-tema-liferay/js/app/app.html'
+})
+export class AppComponent {
+
+	caption = 'hola mutual!';
+
+	constructor() {
+	}
+
+}
